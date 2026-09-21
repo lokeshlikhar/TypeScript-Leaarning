f96e55154bad7c444c036d74eac7c6588e7109d3
lokeshlikhar <lokeshlikhar01@gmail.com>
2026-09-21 13:57:47 +0530

    learn datat types

--- dist/datatypes.js
+++ dist/datatypes.js
@@ -14,12 +14,57 @@ let isvalid = true;
 //deeply with number data type
 //there is same way of initilizing integer , negative number , decimal number 
 //special types in number
-//NaN -> comes when calculation is invalid
-let result = 0 / 0;
+let result = 0 / 0; //NaN -> comes when calculation is invalid
 console.log(result); //NaN
 //Infinity
 let r = 10 / 0;
 console.log(r); //Infinity
-//Min and max value
-console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER); //Min and max value
 console.log(Number.MIN_SAFE_INTEGER);
+//String data type -> 3 ways to declared
+let firstName = "Lokesg";
+let middleName = 'Nitin';
+let lastName = `Likhar`;
+console.log(firstName, lastName, middleName);
+console.log(firstName + lastName + middleName);
+//boolean
+let isLoggedIn = true; //same like false
+//null and undefined 
+let userProfile = null; //null means internally empty value .. explicit value .. user assign it explicitly..it is object
+let surname; //here declaration done ..but not having any value ..its type is primitive type
+//any and unknow 
+let data = "lokesh";
+data = 34; //when used any .. means any type ..we can change its data type just like JS
+let value = "kalpana"; // type-safe counterpart to any . unknow means ..jiski type ab tk pta nhi hai .. jb type pta chlegi tb koni kaam perform hoga
+// console.log(value.toUpperCase()); // it will give error 
+if (typeof value == "string") { //phle type check krega 
+    console.log(value.toUpperCase());
+}
+//never and void data type
+//never means ..esi value jo kbhi exist hi nhi krti ..also when function is not completed .. and throwing err ..it does not return anything 
+// function throwError(message : string):never{
+//     throw new Error(message);
+// }
+// throwError("my error");
+//void ..means function get completed but not returning any value 
+function v() {
+    console.log("hello");
+}
+v();
+//BigInt data type ..just like long in java but it has no limit and not support decimal ..last me n lgana jruri hai
+//operation cannot be done betwn number and bigint.. also we can asisgn 651 digit in bignit
+const bigNum = 12345678901234567890n;
+console.log(typeof bigNum);
+const b = BigInt(2723782328);
+console.log(bigNum, b);
+//symbol data type
+//it is primitive data type ..that create a unique value
+let id = Symbol("this is id");
+console.log(id); // it will print its description  ..description use for debugging
+let id2 = Symbol("this is description");
+const user = {
+    name: "lokesh", //normal key
+    [id]: 123 // string key
+};
+console.log(user[id]);
+console.log(user.name);
--- src/datatypes.ts
+++ src/datatypes.ts
@@ -19,14 +19,72 @@ let isvalid : boolean = true;
 //there is same way of initilizing integer , negative number , decimal number 
 //special types in number
 
-//NaN -> comes when calculation is invalid
-let result:number = 0/0;
+
+let result:number = 0/0;//NaN -> comes when calculation is invalid
 console.log(result); //NaN
 
 //Infinity
 let r:number = 10/0;
 console.log(r); //Infinity
 
-//Min and max value
-console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(Number.MAX_SAFE_INTEGER);//Min and max value
 console.log(Number.MIN_SAFE_INTEGER);
+
+
+//String data type -> 3 ways to declared
+let firstName : string = "Lokesg";
+let middleName : string = 'Nitin';
+let lastName : string = `Likhar`;
+console.log(firstName , lastName , middleName);
+console.log(firstName+lastName+middleName);
+
+
+//boolean
+let isLoggedIn : boolean = true; //same like false
+
+//null and undefined 
+let userProfile : null = null; //null means internally empty value .. explicit value .. user assign it explicitly..it is object
+let surname : string; //here declaration done ..but not having any value ..its type is primitive type
+
+
+//any and unknow 
+let data:any = "lokesh";
+data = 34; //when used any .. means any type ..we can change its data type just like JS
+let value : unknown = "kalpana"; // type-safe counterpart to any . unknow means ..jiski type ab tk pta nhi hai .. jb type pta chlegi tb koni kaam perform hoga
+// console.log(value.toUpperCase()); // it will give error 
+if(typeof value == "string"){ //phle type check krega 
+    console.log(value.toUpperCase());
+}
+
+//never and void data type
+//never means ..esi value jo kbhi exist hi nhi krti ..also when function is not completed .. and throwing err ..it does not return anything 
+// function throwError(message : string):never{
+//     throw new Error(message);
+// }
+// throwError("my error");
+
+//void ..means function get completed but not returning any value 
+function v():void{
+    console.log("hello");
+}
+v();
+
+//BigInt data type ..just like long in java but it has no limit and not support decimal ..last me n lgana jruri hai
+//operation cannot be done betwn number and bigint.. also we can asisgn 651 digit in bignit
+const bigNum: bigint = 12345678901234567890n;
+console.log(typeof bigNum);
+const b = BigInt(2723782328);
+console.log(bigNum , b);
+
+//symbol data type
+//it is primitive data type ..that create a unique value
+let id = Symbol("this is id");
+console.log(id); // it will print its description  ..description use for debugging
+let id2 : symbol = Symbol("this is description");
+const user = {
+    name : "lokesh", //normal key
+    [id] : 123 // string key
+}
+console.log(user[id]);
+console.log(user.name);
